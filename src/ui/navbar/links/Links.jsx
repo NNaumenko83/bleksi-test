0f348719ba70df83c4349590af5eafb67f0118ce
Mykola <nnv2006@ukr.net>
2024-04-09 13:58:28 +0300

    ADD: Links

---
+++ src/ui/navbar/links/Links.jsx
@@ -0,0 +1,57 @@
+'use client';
+import React, { useState } from 'react';
+import styles from './links.module.css';
+import NavLink from './navLink/navLink';
+import Image from 'next/image';
+import { handleLogout } from '@/lib/action';
+
+const links = [
+    {
+        title: 'Homepage',
+        path: '/',
+    },
+    {
+        title: 'Blog',
+        path: '/blog',
+    },
+];
+
+function Links({ session }) {
+    const [open, setOpen] = useState(false);
+    return (
+        <div className={styles.container}>
+            <div className={styles.links}>
+                {links.map(link => (
+                    <NavLink item={link} key={link.path} />
+                ))}
+                {session?.user ? (
+                    <>
+                        <form action={handleLogout}>
+                            <button className={styles.logout}>Logout</button>
+                        </form>
+                    </>
+                ) : (
+                    <NavLink item={{ title: 'Login', path: '/login' }} />
+                )}
+            </div>
+
+            <Image
+                className={styles.menuButton}
+                src="/menu.png"
+                alt=""
+                width={30}
+                height={30}
+                onClick={() => setOpen(!open)}
+            />
+            {open && (
+                <div className={styles.mobileLinks}>
+                    {links.map(link => (
+                        <NavLink item={link} key={link.path} />
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Links;
