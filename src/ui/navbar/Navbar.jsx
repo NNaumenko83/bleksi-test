a3fb07ae84d66f1e330632b32d4b8912f424e607
Mykola <nnv2006@ukr.net>
2024-04-09 13:53:20 +0300

    ADD: Navbar

---
+++ src/ui/navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Links from './links/Links';
+import styles from './navbar.module.css';
+import Link from 'next/link';
+import { auth } from '@/lib/auth';
+
+async function Navbar() {
+    const session = await auth();
+    console.log('session:', session);
+
+    return (
+        <div className={styles.container}>
+            <Link href="/" className={styles.logo}>
+                Bleksi
+            </Link>
+            <div>{<Links session={session} />}</div>
+        </div>
+    );
+}
+
+export default Navbar;
